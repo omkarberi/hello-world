55f7522422aad002da5d188884eb63223b921fd3
omkarberi <omkar_beri@gmail.com>
2021-09-01 21:24:05 +0530

    aadded 2 fn headers

--- math11.py
+++ math11.py
@@ -1,11 +1,13 @@
 #addition function
 def add(x,y):
     pass
+    #sub function
 def subtract(x,y):
     pass
     #multiply function
 def multiply(x,y):
     pass
+    #div function
 def divide(x,y):
  
 
